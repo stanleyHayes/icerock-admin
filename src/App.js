8d7074e8c47247843ff486d1dcd0ad16aae98fb8
Stanley Hayford <hayfordstanley@gmail.com>
2020-07-26 22:55:30 +0000

    Initial commit Navigation implemented with routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import ScrollToTop from "./components/shared/ScrollToTop";
+import DashboardPage from "./pages/dashboard/DashboardPage";
+import AccountPage from "./pages/account/AccountPage";
+import ArchivePage from "./pages/archive/ArchivePage";
+import ChangePasswordPage from "./pages/authentication/ChangePasswordPage";
+import OrderDetailPage from "./pages/orders/OrderDetailPage";
+import OrdersPage from "./pages/orders/OrdersPage";
+import WhatsNewPage from "./pages/whatsnew/WhatsNewPage";
+import ProductsPage from "./pages/products/ProductsPage";
+import ProductDetailPage from "./pages/products/ProductDetailPage";
+import UpdateProductPage from "./pages/products/UpdateProductPage";
+import CreateProductPage from "./pages/products/CreateProductPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <ScrollToTop>
+          <Switch>
+            <Route path="/account" exact={true}>
+              <AccountPage />
+            </Route>
+
+            <Route path="/archive" exact={true}>
+              <ArchivePage />
+            </Route>
+
+            <Route path="/auth/change-password" exact={true}>
+              <ChangePasswordPage />
+            </Route>
+
+            <Route path="/auth/forgot-password" exact={true}>
+              <DashboardPage />
+            </Route>
+
+            <Route path="/auth/reset-password" exact={true}>
+              <DashboardPage />
+            </Route>
+
+            <Route path="/auth/login" exact={true}>
+              <DashboardPage />
+            </Route>
+
+            <Route path="/auth/register" exact={true}>
+              <DashboardPage />
+            </Route>
+
+            <Route path="/" exact={true}>
+              <DashboardPage />
+            </Route>
+
+            <Route path="/orders/:orderId" exact={true}>
+              <OrderDetailPage />
+            </Route>
+
+            <Route path="/orders" exact={true}>
+              <OrdersPage />
+            </Route>
+
+            <Route path="/products" exact={true}>
+              <ProductsPage />
+            </Route>
+
+            <Route path="/products/:productId" exact={true}>
+              <ProductDetailPage />
+            </Route>
+
+            <Route path="/products/:productId/edit" exact={true}>
+              <UpdateProductPage />
+            </Route>
+
+            <Route path="/new/product" exact={true}>
+              <CreateProductPage />
+            </Route>
+
+            <Route path="/whatsnew" exact={true}>
+              <WhatsNewPage />
+            </Route>
+          </Switch>
+        </ScrollToTop>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/products/CreateProductPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import {Container} from "@material-ui/core";
+
+function CreateProductPage() {
+    return (
+        <Layout>
+            <Container>
+                <p className="text-align-center font-weight-bold font-size-large">Create Product Page</p>
+            </Container>
+        </Layout>
+    )
+}
+
+export default CreateProductPage;
