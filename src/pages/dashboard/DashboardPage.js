ac1f8c1c63e07794a96e75a69c6c14646ca86947
Stanley Hayford <hayfordstanley@gmail.com>
2020-08-02 18:55:46 +0000

    Final commit

--- src/pages/dashboard/DashboardPage.js
+++ src/pages/dashboard/DashboardPage.js
@@ -1,12 +1,55 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
-import {Container} from "@material-ui/core";
+import {CardContent, Container, Grid, Card, Avatar} from "@material-ui/core";
 
 function DashboardPage() {
     return (
         <Layout>
             <Container>
-                <p className="text-align-center font-weight-bold font-size-large">Dashboard Page</p>
+                <p className="font-weight-bold font-size-large uppercase">Dashboard</p>
+                <Grid container={true} spacing={3}>
+                    <Grid item={true} xs={12} md={4}>
+                        <Card raised={true} elevation={1} variant="elevation">
+                            <CardContent>
+                                <Grid container={true} justify="center">
+                                    <Grid item={true}>
+                                        <img width="30" height="30" alt="Orders logo"src={`${process.env.PUBLIC_URL}/images/pin.svg`} />
+                                    </Grid>
+                                </Grid>
+                                <p className="uppercase text-align-center font-weight-bold font-size-medium">Messages</p>
+                                <p className=" text-align-center font-size-small">5 Messages</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item={true} xs={12} md={4}>
+                        <Card raised={true} elevation={1} variant="elevation">
+                            <CardContent>
+                                <Grid container={true} justify="center">
+                                    <Grid item={true}>
+                                        <img width="30" height="30" alt="Orders logo" src={`${process.env.PUBLIC_URL}/images/menu.svg`} />
+                                    </Grid>
+                                </Grid>
+                                <p className="uppercase text-align-center font-weight-bold font-size-medium">Orders</p>
+                                <p className=" text-align-center font-size-small">50 Orders</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item={true} xs={12} md={4}>
+                        <Card raised={true} elevation={1} variant="elevation">
+                            <CardContent>
+                                <Grid container={true} justify="center">
+                                    <Grid item={true}>
+                                        <img width="30" height="30" alt="Orders logo" src={`${process.env.PUBLIC_URL}/images/review.svg`} />
+                                    </Grid>
+                                </Grid>
+                                <p className="uppercase text-align-center font-weight-bold font-size-medium">Reviews</p>
+                                <p className=" text-align-center font-size-small">50 Reviews</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </Container>
         </Layout>
     )
